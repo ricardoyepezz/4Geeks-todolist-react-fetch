dfe6806a9ff09170fc7b1f8123b3a038c9ea8ed6
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-19 15:48:01 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,41 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
+/*fetch("https://hp-api.herokuapp.com/api/characters")
+.then((response) => response.json())
+.then((result) => {
+	setHp(result);
+})
+.catch((error) => console.log("error", error));
+}, []); */
+
 const Home = () => {
+	const [task, setTask] = useState();
+	useEffect(() => {
+		var requestOptions = {
+			method: "GET",
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/ricardoyepez",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				setTask(result);
+			})
+			.catch((error) => console.log("error", error));
+	}, []);
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container border border-dark text-center w-50 mt-5">
+			<h1 className="fw-light">To-Do List</h1>
+			<h3 className="fw-light">Tasks:</h3>
+			<div className="text-center">
+				{task?.map((e, index) => {
+					return <li key={index}>{e.label}</li>;
+				})}
+			</div>
 		</div>
 	);
 };
