650f39eb5468946c5a890f08d9712a1a254b9fad
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-21 22:10:01 +0000

    exercise done, missing styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,46 +1,104 @@
 import React, { useState, useEffect } from "react";
-import FormTask from "./formTask";
-import Task from "./task";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Home = () => {
+	// creo estados useState
 	const [task, setTask] = useState([]);
+
+	// utilizo hook useEffect para renderizar valores en GET------------------------------------------------
+
 	useEffect(() => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/ricardoyepez")
-			.then((response) => response.json())
-			.then((result) => {
-				setTask(result);
-			})
-			.catch((error) => console.log("error", error));
+		getData();
 	}, []);
 
-	function addTask() {
+	// funcion para traer tasks de api (GET)----------------------------------------------------------------
+
+	function getData() {
 		var requestOptions = {
-			method: "PUT",
+			method: "GET",
 			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(task),
 		};
+
 		fetch(
 			"https://assets.breatheco.de/apis/fake/todos/user/ricardoyepez",
 			requestOptions
 		)
 			.then((response) => response.json())
-			.then((result) => console.log(result))
+			.then((result) => {
+				console.log(result);
+				setTask(result);
+			})
 			.catch((error) => console.log("error", error));
+	}
+
+	// funcion para agregar tasks a api (PUT)----------------------------------------------------------------
+	function addTask(e) {
+		if (e.key === "Enter" && e.target.value !== "") {
+			//si pulso enter al introducir texto en input agrega value a label y false a done
+			const addNewTask = { label: e.target.value, done: false };
+			//luego concatena nuevo array addNewTask en mi estado
+			let newArray = task.concat(addNewTask);
+			//ejecuta metodo PUT con nuevo array (agrega nuevo array a api)
+			putData(newArray);
+			//deja en blanco el input
+			e.target.value = "";
+		}
+	}
+	function putData(newArray) {
+		var requestOptions = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newArray),
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/ricardoyepez",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {})
+			.catch((error) => console.log("error", error));
+	}
+	// funcion para eliminar tasks de api (PUT)----------------------------------------------------------------
+	function delTask(index) {
+		if (index > -1) {
+			//REVISARRRRRRRRRRRRRRRRR
+			const filterData = task.filter((item) => item !== task[index]);
+			setTask(filterData);
+			putData(filterData);
+		}
 	}
 
 	return (
 		<div className="container border border-dark text-center w-50 mt-5">
 			<h1 className="fw-light">To-Do List</h1>
-			<h3 className="fw-light">Tasks:</h3>
-			<div className="text-center">
-				<FormTask addTask={addTask} setTask={setTask} task={task} />
-				{task?.map((e, index) => {
-					<Task key={index} index={index} todo={e} />;
-					/* return <li key={index}>{e.label}</li>; */
-				})}
+			<h3 className="fw-light">Tasks: {task.length}</h3>
+			<div className="input-group mb-3">
+				<input
+					type="text"
+					placeholder="add a task!"
+					onKeyPress={(e) => {
+						addTask(e);
+					}}
+				/>
 			</div>
+
+			{task.map((element, index) => {
+				return (
+					<div
+						key={index}
+						onClick={() => {
+							delTask(index);
+						}}>
+						{element.label}{" "}
+						<span>
+							{" "}
+							<FaTrashAlt />{" "}
+						</span>{" "}
+					</div>
+				);
+			})}
 		</div>
 	);
 };
-
 export default Home;
